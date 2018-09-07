c077a8c2e5738b47cb301f07806af5a4c6714dff
GolamSaroarGitHub <golamsaroar89@gmail.com>
2018-09-07 15:14:25 +0600

    Update qtest_test.py

--- API/qtest_test.py
+++ API/qtest_test.py
@@ -4,11 +4,11 @@ import json
 import requests
 import pprint
 
-accessToken= 'dGVzdHJlYWNodWl8aW5mb0B0ZXN0cmVhY2guY29tOjE1MTE0NjM2NTkyNjI6MDU0NDYwNzA2N2U4NzBhNTY4Nzg5ZmNmODg0ZjJjZDk'
+accessToken= 'dGVzdHJlYWNodWl8aW5mb0B0ZXN0cmVhY2guYdfgdsf29tOjE1MTE0NjM2NTkyNjI6MDU0NDYwNzA2N2U4NzBhNTY4Nzg5ZmNmODg0ZjJjZDk'
 url = 'https://testreachui.qtestnet.com/api/v3/projects/32348/test-cycles/278131'
 
 headers = {'Content-type': 'application/json',
-           'Authorization': 'dGVzdHJlYWNodWl8aW5mb0B0ZXN0cmVhY2guY29tOjE1MTE0NjM2NTkyNjI6MDU0NDYwNzA2N2U4NzBhNTY4Nzg5ZmNmODg0ZjJjZDk'
+           'Authorization': 'dGVzdHJlYWNodWl8aW5mb0B0ZXN0cmVhY2guY29sdgtOjE1MTE0NjM2NTksdfI6MDU0NDYwNzA2N2U4NzBhNTY4Nzg5ZmNmODg0ZjJjZDk'
            }
 
 response = requests.get(url, headers=headers)
